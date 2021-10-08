9d8e426b0f9527592796b8b6eda750f9cb632821
Konstantine <korolenko.kg@gmail.com>
2021-10-08 16:06:46 +0300

    blog version 1.1

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def admin_only(f):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
